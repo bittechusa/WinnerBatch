596bb0d0d9075249828c98cc19e7d005ef4930ec
Bittech <you@example.com>
2016-03-13 13:18:40 -0400

    add project

---
+++ Access Modifier080815/src/com/bit/suopport/C.java
@@ -0,0 +1,24 @@
+package com.bit.suopport;
+
+public class C
+{
+	static void m5()
+	{
+		System.out.println("gdjfhg");
+		System.out.println("gfhfh");
+	}
+	static public void m6()
+	{
+		
+	}
+	static protected void m7()
+	{
+		
+	}
+	static private void m8()
+	{
+		
+	}
+
+
+}
